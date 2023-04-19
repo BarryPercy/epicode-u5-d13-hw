8fd5786638053121cfa39b61c70ee5b0cc183a28
Barry Percy <baz.percy@gmail.com>
2023-04-19 11:41:46 +0100

    finished

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,13 +6,49 @@ import {
   Form,
   FormControl,
   ListGroup,
-  Button,
 } from "react-bootstrap"
+import { io } from 'socket.io-client'
+import { User, Message } from '../types'
 
 const Home = () => {
   const [username, setUsername] = useState("")
   const [message, setMessage] = useState("")
+  const [onlineUsers, setOnlineUsers] = useState<User[]>([])
+  const [loggedIn, setLoggedIn] = useState(false)
+  const [chatHistory, setChatHistory] = useState<Message[]>([])
 
+  const socket = io("http://localhost:3001", { transports: ['websocket'] })
+
+  useEffect(() => {
+    socket.on("welcome", welcomeMessage => {
+      socket.on("loggedIn", onlineUsersList => {
+        console.log(onlineUsersList)
+        setOnlineUsers(onlineUsersList)
+      })
+      socket.on("updateOnlineUsersList", updatedList => {
+        setOnlineUsers(updatedList)
+      })
+      socket.on("newMessage", newMessage => {
+        console.log(newMessage)
+        setChatHistory((chatHistory) => [...chatHistory, newMessage.message])
+      })
+    })
+  }, [])
+
+  const submitUsername = () => {
+    socket.emit("setUsername", { username })
+    setLoggedIn(true);
+  }
+
+  const sendMessage = () => {
+    const newMessage = {
+      sender: username,
+      text: message,
+      createdAt: new Date().toLocaleString("en-GB")
+    }
+    socket.emit("sendMessage", { message: newMessage })
+    // setChatHistory([...chatHistory, newMessage])
+  }
   return (
     <Container fluid>
       <Row style={{ height: "95vh" }} className="my-3">
@@ -23,33 +59,43 @@ const Home = () => {
           <Form
             onSubmit={e => {
               e.preventDefault()
+              submitUsername()
             }}
           >
             <FormControl
               placeholder="Set your username here"
               value={username}
               onChange={e => setUsername(e.target.value)}
+              disabled={loggedIn}
             />
           </Form>
           {/* )} */}
-          {/* MIDDLE AREA: CHAT HISTORY */}
+            {chatHistory.map((message, index) => (<ListGroup.Item key={index}>{<strong>{message.sender} </strong>} | {message.text} at {message.createdAt}</ListGroup.Item>))}
           <ListGroup></ListGroup>
           {/* BOTTOM AREA: NEW MESSAGE */}
           <Form
             onSubmit={e => {
               e.preventDefault()
+              sendMessage()
             }}
           >
             <FormControl
               placeholder="Write your message here"
               value={message}
               onChange={e => setMessage(e.target.value)}
+              disabled={!loggedIn}
             />
           </Form>
         </Col>
         <Col md={3}>
           {/* ONLINE USERS SECTION */}
           <div className="mb-3">Connected users:</div>
+          {onlineUsers.length === 0 && (
+            <ListGroup.Item>Log in to check who's online!</ListGroup.Item>
+          )}
+          <ListGroup>
+            {onlineUsers.map(user => (<ListGroup.Item key={user.id}>{user.username}</ListGroup.Item>))}
+          </ListGroup>
         </Col>
       </Row>
     </Container>
